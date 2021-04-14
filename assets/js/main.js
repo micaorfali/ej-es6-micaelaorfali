1ff100e81784d90d3caf34a5d086785955265d60
micaorfali <mica.orfali@gmail.com>
2021-04-14 18:20:56 -0300

    archivos base

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*Javascript ES6, today is the day....*/
+
+// ECMAScript o ES, es un conjunto de especificaciones que salen año tras año.
+// ECMAScript6 o ES2015, fue el conjunto de features más grande hasta el momento.
+
+/*
+1) Const y Let (Block Scope)
+2) Arrow Functions
+3) Modules (Imports and Exports)
+4) Template Literals
+5) Classes (Constructor, Super, Setters and Getters)
+6) Default Parameters
+7) The Spread Operator
+8) Destructuring
+9) Rest Operator
+10) map(), filter(), reduce() 
+11) Promesas, Async/Await
+*/
+
